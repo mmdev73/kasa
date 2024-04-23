d65e1e86c6bf68b511ad78137ba83f293a4d27b8
mmdev73 <mm.dev73@gmail.com>
2024-04-23 11:56:06 +0200

    :tada: First commit - tree structure

---
+++ src/data/collapses.ts
@@ -0,0 +1,23 @@
+import { CollapseInterface } from '../assets/interfaces'
+export const collapses:CollapseInterface[] = [
+    {
+        id: 0,
+        title:"Fiablilité",
+        content:"Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."
+    },
+    {
+        id: 1,
+        title:"Respect",
+        content:"La bienveillance fait partie des valeurs fondatrice de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme"
+    },
+    {
+        id: 2,
+        title:"Service",
+        content:"La bienveillance fait partie des valeurs fondatrice de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme"
+    },
+    {
+        id: 3,
+        title:"Sécurité",
+        content:"La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standars sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+    }
+]
